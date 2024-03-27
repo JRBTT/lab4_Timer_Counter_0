e88c0b09520b06f7b43598282f390e8f3185a25f
JRBTT <jason.ray.buntting@gmail.com>
2024-03-27 23:15:46 +1100

    from slides

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,47 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include <math.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <avr/interrupt.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+//#include "usart.h"
+//#include "bit.h"
+// bit register manipulation
+#define bitSet(reg, n) (reg |= 1 << n)
+#define bitRead(reg, n) (reg & (1 << n))
+#define bitClear(reg, n) (reg &= ~(1 << n))
+
+# define CS00 0
+
+// delay of 1 second
+void my_delay_1e6us()
+{
+  unsigned long num0V_max = 62500;
+
+  num0V = 0;
+  TCNT0 = 0;
+  bitSet(TCCR0B, CS00);
+  while (num0V < num0V_max - 1);
+  bitClear(TCCR0B, CS00);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+int setPrescaler_tc0(char option);
+void my_delay_ms(unsigned long x);
+
+void my_delay_1e6us();
+
+volatile unsigned long num0V = 0;
+
+ISR(TIMER0_OVF_vect)
+{
+  num0V++;
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int main()
+{
+  //usart_init(8); // 103-9600 bps; 8-115200
+
+  bitSet(TIMSK0, TOIE0); // enable timer0 overflow interrupt
+  sei();
 }
