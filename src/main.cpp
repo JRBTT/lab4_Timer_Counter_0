62dc18a1021ba329480a27cf4e6a0d39324255d3
JRBTT <jason.ray.buntting@gmail.com>
2024-03-28 13:31:29 +1100

    lab 4 complete

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,19 @@
 #include "usart.h"
 #include "bit.h"
 
-# define CS00 0
-
 #define FOSC 16000000 // Frequency Oscillator 16Mhz for Uno R3
 #define BAUD 9600 // 9600 Bits per second
 #define MYUBRR FOSC / 16 / BAUD - 1 // My USART Baud Rate Register
+#define PRESCALER 3 //250 KHz
+#define DELAY 200 // 200 ms
+
+#define PORT &PORTB
+#define DDR &DDRB
+#define PIN PINB0
 
 int setPrescaler_tc0(char option);
-//void my_delay_ms(unsigned long x);
+int setMax_count_tc0(int num);
+void my_delay_ms(unsigned long x);
 void my_delay_1e6us();
 volatile unsigned long num0V = 0;
 
@@ -25,20 +30,33 @@ void my_delay_1e6us()
   TCNT0 = 0; // timer0 counter register, sets count to 0
   //bitSet(TCCR0B, CS00); // starts timer0
   setPrescaler_tc0(2);
-  // repeatedly counts to 255 and overflows triggering the ISR to increment.
+  // repeatedly counts to 255 and overflows at 256 triggering the ISR to increment.
   while (num0V < num0V_max - 1); 
   //bitClear(TCCR0B, CS00); // stops timer0
   setPrescaler_tc0(0);
 }
 
+int setMax_count_tc0(int num)
+{
+  int prescalers[6] = {0, 1, 8, 64, 256, 1024};
+  float overflow_count = ((1 / ((float)FOSC / prescalers[PRESCALER])) * 256);
+  float time_sec = (float)num / 1000;
+  float max = (time_sec / overflow_count);
+  int max_count = (int)round(max);
+  return max_count;
+}
 
-// void my_delay_ms(unsigned long x)
-// {
-//   for (unsigned long i = 0; i < x; i++)
-//   {
-//     my_delay_1e6us();
-//   }
-// }
+// custom delay with prescalers
+void my_delay_ms(unsigned long x)
+{
+  unsigned long num0V_max = setMax_count_tc0(x);
+  num0V = 0; // timer0 overflow counter, sets count to 0
+  TCNT0 = 0; // timer0 counter register, sets count to 0
+  setPrescaler_tc0(PRESCALER);
+  while (num0V < num0V_max - 1); 
+  
+  setPrescaler_tc0(0);
+}
 
 
 int setPrescaler_tc0(char option)
@@ -84,7 +102,7 @@ int setPrescaler_tc0(char option)
   default:
     return -1;
   }
-  return 0;
+  return option;
 }
 
 ISR(TIMER0_OVF_vect)
@@ -97,18 +115,20 @@ int main()
   usart_init(MYUBRR); // 103-9600 bps; 8-115200
   bitSet(TIMSK0, TOIE0); // enable timer0 overflow interrupt
   sei(); // enable global interrupts
+  bitSet(*DDR, PIN); // set pin 8 as output
+
+
   while (1)
   {
-    usart_tx_string(">a:");
-    usart_transmit('1');
-    usart_transmit('\n');
-
-    my_delay_1e6us();
-
-    usart_tx_string(">a:");
-    usart_transmit('0');
-    usart_transmit('\n');
-
-    my_delay_1e6us();
+    bitSet(*PORT, PIN);
+    // usart_tx_string("a:>");
+    // usart_transmit('1');
+    // usart_transmit('\n');
+    my_delay_ms(DELAY);
+    bitClear(*PORT, PIN);
+    // usart_tx_string("a:>");
+    // usart_transmit('0');
+    // usart_transmit('\n');
+    my_delay_ms(DELAY);
   }
 }
